a9ec7d198889504d929aeedb3bbdd64576754eb1
Eren B <86406235+Erenarius@users.noreply.github.com>
2023-08-19 15:06:24 +0300

    Delete hanzade directory

--- hanzade/xcn_baglanti/baglan.php
+++
@@ -1,7 +0,0 @@
-<?php
-try {
-	 $db = new PDO("mysql:host=localhost;dbname=kazandi6_am5;charset=utf8", "kazandi6_am5", "~f3G$9R7GNwj");
-} catch ( PDOException $e ){
-     print $e->getMessage();
-}
-?>
--- hanzade/yonetim/cikis.php
+++
@@ -1,8 +0,0 @@
-<?php
-ob_start(); 
-$cookiePath = "/";
-setcookie("scriptindirnet_satisscriptv1","", time()-3600, $cookiePath);
-unset ($_COOKIE['scriptindirnet_satisscriptv1']);
-echo '<meta http-equiv="refresh" content="0;URL=index.php">'; 
-ob_end_flush(); 
-?>
